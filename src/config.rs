1f70398eb4282b27e2177be4e95e0aeddd072d00
sengeer <fox8911@gmail.com>
2026-05-01 23:22:31 +0300

    фича: реализовать модуль config и систему логирования в main

---
+++ src/config.rs
@@ -0,0 +1,29 @@
+// Модуль env из стандартной библиотеки Rust для переменных окружения
+use std:: env;
+
+// Публичный API модуля
+pub struct Settings {
+    pub bot_token: String,
+    pub database_url: String,
+}
+
+// Имплементация типа Settings
+impl Settings {
+    // Метод для получения переменных окружения
+    pub fn from_env() -> anyhow::Result<Self> {
+        // Пытается получить BOT_TOKEN, если не удаётся - замыкание с сообщением
+        let bot_token = env::var("BOT_TOKEN")
+            .map_err(|_| anyhow::anyhow!("BOT_TOKEN environment variable is required"))?;
+        // Пытается получить DATABASE_URL, если не удаётся - замыкание с fallback адресом
+        let database_url =
+            env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite://rusty_key.db".to_string());
+
+        // Возврат результата
+        Ok(Self {
+            bot_token,
+            database_url,
+        })
+    }
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+// Объявления модулей
+mod config;
+
+// Импорты API из крейтов
+use tracing::{error, info};
+use tracing_subscriber::{EnvFilter, fmt};
+
+// runtime tokio через атрибут-макрос
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Инициализация системы логирования
+    init_tracing();
+    // Использование типа Settings из модуля config
+    let settings = config::Settings::from_env()?;
+
+    Ok(())
+}
+
+// Система логирования
+fn init_tracing() {
+    // Создаёт фильтр
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    // Попытка инициализировать логгер, если не получается то выкидывает ошибку
+    if let Err(err) = fmt().with_env_filter(filter).try_init() {
+        error!("failed to init tracing: {err}");
+    }
 }
